830697446f177f6dede59577236346c4713bdb4a
BooguNote <boogunote@gmail.com>
2016-10-22 17:45:38 +0800

    clean vm page children

--- app/containers/VmPage/reducer.js
+++ app/containers/VmPage/reducer.js
@@ -27,147 +27,13 @@ import {
 
 import { fromJS } from 'immutable';
 
+import { combineReducers } from 'redux'
+
 // The initial state of the App
 const initialState = fromJS({
 });
 
-// function list(state = initialState, action) {
-function list(state = [],
-action) {
-  switch (action.type) {
-    case PAGE_VM_UPDATE_LIST: {
-      return Object.assign([], action.items);
-    }
-    case PAGE_VM_LIST_HIGHLIGHT: {
-      let newList = Object.assign([], state);
-      action.uuidList.forEach(function(uuid) {
-        for (var i in newList) {
-          if (newList[i].uuid == uuid) {
-            newList[i].highlight = true;
-            break;
-          }
-        }
-      })
-      return newList;
-    }
-    case PAGE_VM_LIST_NORMAL: {
-      let newList = Object.assign([], state);
-      action.uuidList.forEach(function(uuid) {
-        for (var i in newList) {
-          if (newList[i].uuid == uuid) {
-            newList[i].highlight = false;
-            break;
-          }
-        }
-      })
-      return newList;
-    }
-    default:
-      return state;
-  }
-}
-
-import { combineReducers } from 'redux'
-import {SHOW_MODAL, HIDE_MODAL, SET_NAME} from './actions'
- 
-function modals(state = {
-  isShowing: false,
-  message: '',
-  name: ''
-}, action) {
- 
-  switch (action.type) {
-    case SHOW_MODAL:
-      return Object.assign({}, state, {
-        isShowing: true,
-        message: action.message,
-        name: state.name
-      })
-    case HIDE_MODAL:
-      return Object.assign({}, state, {
-        isShowing: false,
-        name: state.name
-      })
-    case PAGE_VM_DIALOG_CREATE_VM_UPDATE:
-      let newState = {};
-      newState[action.name] = action.value;
-      return Object.assign({}, state, newState)
-    default:
-      return state
-  }
- 
-}
- 
-function name(state = null, action) {
- 
-  switch (action.type) {
-    case SET_NAME:
-      return action.name
-    default:
-      return state
-  }
- 
-}
-
-function currItemUuid(state = null, action) {
- 
-  switch (action.type) {
-    case PAGE_VM_SHOW_DETAIL:
-      return action.uuid
-    case PAGE_VM_HIDE_DETAIL:
-      return null
-    default:
-      return state
-  }
- 
-}
-
-function pageSize(state = 20, action) {
-  switch (action.type) {
-    case PAGE_VM_LIST_SET_PAGE_SIZE:
-      return action.size
-    default:
-      return state
-  }
-}
-
-function pageNumber(state = 1, action) {
-  switch (action.type) {
-    case PAGE_VM_LIST_SET_PAGE_NUMBER:
-      return action.number
-    default:
-      return state
-  }
-}
-
-function count(state = 0, action) {
-  switch (action.type) {
-    case PAGE_VM_LIST_SET_ITEM_COUNT:
-      return action.count
-    default:
-      return state
-  }
-}
-
-function showCreateDialog(state = false, action) {
-  switch (action.type) {
-    case PAGE_VM_SHOW_CREATE_VM_DIALOG:
-      return action.show
-    default:
-      return state
-  }
-}
-
-function detailSidePageUuid(state = false, action) {
-  switch (action.type) {
-    case PAGE_VM_SET_DETAIL_SIDE_PAGE_UUID:
-      return action.uuid
-    default:
-      return state
-  }
-}
-
-function listWindowUuid(state = false, action) {
+function listWindowUuid(state = '', action) {
   switch (action.type) {
     case PAGE_VM_SET_LIST_WINDOW_UUID:
       return action.uuid
@@ -177,15 +43,6 @@ function listWindowUuid(state = false, action) {
 }
 
 export default combineReducers({
-  list,
-  modals,
-  name,
-  currItemUuid,
-  pageSize,
-  pageNumber,
-  count,
-  showCreateDialog,
-  detailSidePageUuid,
   listWindowUuid
 })
 
