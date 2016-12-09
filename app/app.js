29a4b0e41d8af89e17b8670a9469bf4cdd0d3a54
BooguNote <boogunote@gmail.com>
2016-12-09 09:03:04 +0800

    [FIX] swtich to hashHistory

--- app/app.js
+++ app/app.js
@@ -21,7 +21,7 @@ import 'font-awesome/css/font-awesome.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { applyRouterMiddleware, Router, Route, IndexRedirect, browserHistory } from 'react-router';
+import { applyRouterMiddleware, Router, Route, IndexRedirect, hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import FontFaceObserver from 'fontfaceobserver';
 import useScroll from 'react-router-scroll';
@@ -50,7 +50,7 @@ import { translationMessages } from './i18n';
 // Optionally, this could be changed to leverage a created history
 // e.g. `const browserHistory = useRouterHistory(createBrowserHistory)();`
 const initialState = {};
-const store = configureStore(initialState, browserHistory);
+const store = configureStore(initialState, hashHistory);
 
 // If you use Redux devTools extension, since v2.0.1, they added an
 // `updateStore`, so any enhancers that change the store object
@@ -65,7 +65,7 @@ if (window.devToolsExtension) {
 // is under the non-default key ("routing"), selectLocationState
 // must be provided for resolving how to retrieve the "route" in the state
 import { selectLocationState } from 'containers/App/selectors';
-const history = syncHistoryWithStore(browserHistory, store, {
+const history = syncHistoryWithStore(hashHistory, store, {
   selectLocationState: selectLocationState(),
 });
 
